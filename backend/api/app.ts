156561c988c7578c11289cf9f2ffc94ae6f881cc
Aaron Buxbaum <me@aaronbuxbaum.com>
2020-07-24 16:59:15 -0400

    Serverless

--- backend/api/app.ts
+++ backend/api/app.ts
@@ -1,4 +1,19 @@
-import { use } from "nexus";
+import app, { server, use } from "nexus";
 import { prisma } from "nexus-plugin-prisma";
+import * as serverless from "serverless-http";
 
 use(prisma({ features: { crud: true } }));
+
+app.assemble();
+
+export const graphql = serverless(server.handlers.graphql);
+/*, {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  request(request: any, event: any) {
+    const { body } = request;
+    request.context = event.requestContext;
+    request.body = JSON.parse(body.toString());
+    return request;
+  },
+});*/
+export const playground = serverless(server.handlers.playground);
