a321de9b2e46bdec86c17e6d9878f297770b11b1
Aaron Buxbaum <me@aaronbuxbaum.com>
2020-07-25 00:27:50 -0400

    Fixed it up

--- backend/api/app.ts
+++ backend/api/app.ts
@@ -1,6 +1,7 @@
 import app, { server, use } from "nexus";
 import { prisma } from "nexus-plugin-prisma";
 import * as serverless from "serverless-http";
+import "./graphql/User"; // hack to resolve https://github.com/graphql-nexus/nexus/issues/782
 
 use(prisma({ features: { crud: true } }));
 
